9776d773869abed8e567e218df2ef1f0ae2ae09d
Nate Tan <nktan93@gmail.com>
2019-04-01 14:24:21 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,28 @@
+require 'pry'
 
+def sort_array_asc(integer_array)
+  integer_array.sort
+end
+
+def sort_array_desc(integer_array)
+  integer_array.sort {|x, y| y <=> x}
+end
+
+def sort_array_char_count(string_array)
+  string_array.sort {|x, y| x.length <=> y.length}
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array(array)
+  i = 0
+  until i == array.length
+    new_array = []
+    new_array << array.last
+    i += 1
+  end
+  new_array
+end
