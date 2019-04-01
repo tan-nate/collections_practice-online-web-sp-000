3ee95521671f1544bf428907e14161d335a0a06b
Nate Tan <nktan93@gmail.com>
2019-04-01 14:36:20 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -19,7 +19,7 @@ end
 
 def reverse_array(array)
   i = 0
-  initial_length = array.length
+  #initial_length = array.length
   new_array = []
   until i == initial_length
     new_array << array.pop
